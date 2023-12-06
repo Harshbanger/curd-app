07c2b26d12d5f598fc8b3c8b95d468c7bb575055
Harshbanger <131772882+Harshbanger@users.noreply.github.com>
2023-12-06 17:22:59 +0530

    g

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: Splashscreen(),
